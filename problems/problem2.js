0b746e9fcc32b292aa1bb8b0c77176387ef2ba54
sulochana <nagasulochanab53@gmail.com>
2024-02-20 11:03:28 +0530

    created for problem2.js

--- problems/problem2.js
+++ problems/problem2.js
@@ -2,13 +2,13 @@
 // The dealer needs the information on the last car in their inventory. Execute a function to find what the make and model of the last car in the inventory is?  Log the make and model into the console in the format of:
 "Last car is a *car make goes here* *car model goes here*";
 
-const inventory = require("./data");
-
 function getLastCar(inventory) {
-  let lastCar = inventory[inventory.length - 1];
-  for (let cars of inventory) {
-    if (cars === lastCar) {
-      return cars;
+  if (Array.isArray(inventory)) {
+    let lastCar = inventory[inventory.length - 1];
+    for (let cars of inventory) {
+      if (cars === lastCar) {
+        return cars;
+      }
     }
   }
 }
