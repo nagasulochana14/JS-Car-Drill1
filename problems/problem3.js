9ecedf19fe2e3bedc6a2e7c56c9d5442f2c7aa13
sulochana <nagasulochanab53@gmail.com>
2024-02-20 11:03:41 +0530

    created for problem3.js

--- problems/problem3.js
+++ problems/problem3.js
@@ -1,12 +1,12 @@
 // ==== Problem #3 ====
 // The marketing team wants the car models listed alphabetically on the website. Execute a function to Sort all the car model names into alphabetical order and log the results in the console as it was returned.
 
-const inventory = require("./data");
-
 function sortCarModelsAlphabetically(inventory) {
   let carModels = [];
-  for (let index = 0; index < inventory.length; index++) {
-    carModels.push(inventory[index].car_model);
+  if (Array.isArray(inventory)) {
+    for (let index = 0; index < inventory.length; index++) {
+      carModels.push(inventory[index].car_model);
+    }
   }
   return carModels.sort();
 }
