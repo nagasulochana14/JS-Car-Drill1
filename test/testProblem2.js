c1604a0f3e19134792afd8dc07b7a0611a70973a
sulochana <nagasulochanab53@gmail.com>
2024-02-20 10:56:40 +0530

    created for testProblem2.js

--- test/testProblem2.js
+++ test/testProblem2.js
@@ -1,5 +1,5 @@
 const problem2 = require("../problems/problem2");
 const inventory = require("../problems/data");
 
-let lastCar = problem2(inventory);
-console.log(`Last Car is a ${lastCar.car_make} ${lastCar.car_model}`);
+let result = problem2(inventory);
+console.log(`Last Car is a ${result.car_make} ${result.car_model}`);
