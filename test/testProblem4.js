aa0d1713e9194ea2c0d5051f7db8a12a86793597
sulochana <nagasulochanab53@gmail.com>
2024-02-20 11:00:07 +0530

    created for testProblem4.js

--- test/testProblem4.js
+++ test/testProblem4.js
@@ -1,5 +1,6 @@
 const problem4 = require("../problems/problem4");
 const inventory = require("../problems/data");
 
-let result = problem4(inventory);
+const result = problem4(inventory);
 console.log(result);
+ 
